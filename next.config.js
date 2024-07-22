877abf5795fbb2201a52395f756c94dce0e79022
Asharib Ali <asharibarain4@gmail.com>
2024-07-22 12:35:51 +0500

    update

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
         protocol: "https",
         hostname: "res.cloudinary.com",
         port: "",
-        pathname: "/dsksuubyx/**", // Ensure this matches the actual path pattern
+        pathname: "/dsksuubyx/**",
       },
     ],
   },
